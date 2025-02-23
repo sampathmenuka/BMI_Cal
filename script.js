4460ec4f4ab43067782284c5dc1dbcee820cec45
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-02-23 08:15:38 +0530

    js file modified and all fuctions created

--- script.js
+++ script.js
@@ -9,4 +9,28 @@ document.getElementById("bmiForm").addEventListener('submit',function(e){
     const heightInches = parseInt(document.getElementById('height-inches').value);
     const weight = parseInt(document.getElementById('weight').value);
 
+    if(gender && age && heightFeet && heightInches && weight){
+
+        const heightInMeters = ((heightFeet * 12) + heightInches) * 0.0254;  // in meters
+        const bmi = weight / (heightInMeters * heightInMeters);
+        const resultElement = document.getElementById("result");
+
+        let category = '';
+
+        if(bmi < 18.5){
+            category = 'Under Weight';
+        }else if(bmi >= 18.5 && bmi <24.9){
+            category = 'Normal Weight'
+        }else if(bmi >= 25 && bmi <29.9){
+            category = 'Over Weight'
+        }else{
+            category = 'Obese'
+        }
+
+        let resultMassage = 'Your BMI : ' + bmi.toFixed(2) + '<>';
+        resultMassage += 'category:' + category;
+        
+        resultElement.innerHTML = resultMassage;
+    }
+
 });
